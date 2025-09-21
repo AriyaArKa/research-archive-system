ccf23c3bacc11765ae11fc9a207a0baecb77646b
Arka Nath <natharka55@gmail.com>
2025-09-21 15:17:44 +0600

    code running

--- ResearchPaper.java
+++ ResearchPaper.java
@@ -2,5 +2,5 @@ public interface ResearchPaper {
     void view();
     String getAuthorName();
     int getPublicationYear();
-    int test();
+    //int test();
 }
--- main.java
+++ main.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<ResearchPaper> papers = new ArrayList<>();
-        papers.add(new ProxyResearchPaper("AI in Healthcare", "Dr. Smith", 2022));
-        papers.add(new ProxyResearchPaper("Quantum Computing", "Dr. Johnson", 2023));
+        papers.add(new ProxyResearchPaper("Testin 1", "hitlar", 2022));
+        papers.add(new ProxyResearchPaper("Testing 2", "putin", 2023));
         System.out.println("Available Papers:");
         for (ResearchPaper paper : papers) {
             System.out.println(paper.getPublicationYear() + " - " + paper.getAuthorName());
